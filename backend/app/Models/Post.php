0286866f8bbfdbc2c4d383f2567d99c6a3561675
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-01-17 22:28:18 +0700

    base

---
+++ backend/app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'headline',
+        'content',
+        'status',
+        'view',
+        'like',
+        'unlike',
+        'share',
+        'except',
+        'featured_image',
+        'category_id',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'post_category', 'post_id', 'category_id');
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tag', 'post_id', 'tag_id');
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
