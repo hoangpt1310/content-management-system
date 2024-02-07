c75764b6e1c78b0a7a0c3ca746c274668898b27e
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-02-07 14:43:29 +0700

    [Task0207] Fix bug #5

---
+++ backend/database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Category::class;
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->name,
+            'slug' => strtolower(implode('-', explode(' ', $this->faker->name)))
+        ];
+    }
+}
